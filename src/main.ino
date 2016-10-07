9aebaab9302856bc228afef72e5ca9215a1e26b1
Jeff Clement <jeffrey.clement@gmail.com>
2016-10-07 17:46:46 -0600

    refactoring for readability

--- src/main.ino
+++ src/main.ino
@@ -11,10 +11,10 @@
 #include <ESP8266WiFi.h>
 
 // PIN used to reset configuration.  Enables internal Pull Up.  Ground to reset.
-#define PIN_RESET 13 // Labeled D7 on ESP12E DEVKIT V2
+#define PIN_RESET 13       // Labeled D7 on ESP12E DEVKIT V2
 #define RESET_DURATION 30
-#define PS_OUTPUT 12
-#define PS_CHECK 14
+#define PS_OUTPUT 12      // D6
+#define PS_CHECK 14       // D5 (or tie to D6)
 
 #include <NeoPixelBus.h>
 
@@ -48,6 +48,11 @@ char* topic_status;
 char* topic_status_mode;
 char* topic_control;
 
+// temporary variables for incoming messages from MQTT
+#define MAX_MESSAGE_SIZE 1000
+bool hasUnprocessedMessage = false;
+char pendingMessage[MAX_MESSAGE_SIZE] = "";
+
 /* ========================================================================================================
                                            __
                               ______ _____/  |_ __ ________
@@ -66,21 +71,17 @@ void saveConfigCallback()
 void saveSetting(const char* key, WiFiManagerParameter setting) {
   char filename[80] = "/config_";
   strcat(filename, key);
-
   const char *value = setting.getValue();
-
   File f = SPIFFS.open(filename, "w");
   if (f) {
     f.print(value);
   }
   f.close();
-
 }
 
 char* readSetting(const char* key) {
   char filename[80] = "/config_";
   strcat(filename, key);
-
   File f = SPIFFS.open(filename, "r");
   char* output = (char *) malloc(f.size()+1);
   f.readBytes(output, f.size());
@@ -92,7 +93,6 @@ char* readSetting(const char* key) {
 uint readIntSetting(const char* key) {
   char filename[80] = "/config_";
   strcat(filename, key);
-
   File f = SPIFFS.open(filename, "r");
   char* output = (char *) malloc(f.size()+1);
   f.readBytes(output, f.size());
@@ -279,72 +279,109 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
   mqttClient.connect();
 }
 
-bool hasNewPayload = false;
-char newPayload[1000] = "";
-
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
   // if we haven't finished processing the current payload, skip this one
-  if (hasNewPayload) return;
-  strcpy(newPayload, payload);
-  hasNewPayload = true;
+  if (hasUnprocessedMessage) return;
+  // if the message is too large, skip it
+  if (len >= MAX_MESSAGE_SIZE) return;
+  // copy payload and flag that we have one!
+  strcpy(pendingMessage, payload);
+  hasUnprocessedMessage = true;
 }
 
-void processMessage() {
+/* ========================================================================================================
+             __  __                                  _   _                 _ _ _
+            |  \/  | ___  ___ ___  __ _  __ _  ___  | | | | __ _ _ __   __| | (_)_ __   __ _
+            | |\/| |/ _ \/ __/ __|/ _` |/ _` |/ _ \ | |_| |/ _` | '_ \ / _` | | | '_ \ / _` |
+            | |  | |  __/\__ \__ \ (_| | (_| |  __/ |  _  | (_| | | | | (_| | | | | | | (_| |
+            |_|  |_|\___||___/___/\__,_|\__, |\___| |_| |_|\__,_|_| |_|\__,_|_|_|_| |_|\__, |
+                                        |___/                                          |___/
+   ======================================================================================================== */
 
-  char targetModeChar = newPayload[0];
-  char* modePayload = newPayload + 1;
+void terminateCurrentMode()
+{
+  delete(currentMode);
+  currentMode = NULL;
+  currentModeChar = '\0';
+}
 
-  if (targetModeChar != currentModeChar) {
-    // New mode.  Dispose of the current one.
-    delete(currentMode);
-    currentMode = NULL;
-    currentModeChar = '\0';
+void updateMode(char* payload)
+{
+  currentMode->update(payload);
 
-    // create mode, if we can find it
-    switch (targetModeChar) {
-      case 'S':
-        currentMode = new Slide(strip, modePayload);
-        break;
-      case 'T':
-        currentMode = new Twinkle(strip, modePayload);
-        break;
-      case 'P':
-        currentMode = new Percent(strip, modePayload, led_inset_start, led_inset_length);
-        break;
-      case 'A':
-        currentMode = new Ants(strip, modePayload);
-        break;
-      case 'R':
-        currentMode = new Rainbow(strip, modePayload);
-        break;
-      case 'K':
-        currentMode = new Tracker(strip, modePayload);
-        break;
-    }
+  // push status message to MQTT
+  char msg[100] = "Update:";
+  strcat(msg, currentMode->description());
+  mqttClient.publish(topic_status_mode, 2, true, msg);
+}
 
-    if (currentMode != NULL) {
-      currentModeChar = targetModeChar;
+void switchMode(char mode, char* payload)
+{
+  LightMode* newMode;
 
-      // push status message to MQTT
-      char msg[100] = "Switch:";
-      strcat(msg, currentMode->description());
-      mqttClient.publish(topic_status_mode, 2, true, msg);
-    } else {
+  // create mode, if we can find it
+  switch (mode) {
+    case 'S':
+      newMode = new Slide(strip, payload);
+      break;
+    case 'T':
+      newMode = new Twinkle(strip, payload);
+      break;
+    case 'P':
+      newMode = new Percent(strip, payload, led_inset_start, led_inset_length);
+      break;
+    case 'A':
+      newMode = new Ants(strip, payload);
+      break;
+    case 'R':
+      newMode = new Rainbow(strip, payload);
+      break;
+    case 'K':
+      newMode = new Tracker(strip, payload);
+      break;
+    case '0': // OFF
+      digitalWrite(PS_OUTPUT, LOW);
+      delete(currentMode);
+      currentMode = NULL;
+      currentModeChar = '\0';
       mqttClient.publish(topic_status_mode, 2, true, "Mode Cleared");
       strip->ClearTo(RgbColor(0,0,0));
       strip->Show();
-    }
-  } else {
-    if (currentMode != NULL) {
-      currentMode->update(modePayload);
-
-      // push status message to MQTT
-      char msg[100] = "Update:";
-      strcat(msg, currentMode->description());
-      mqttClient.publish(topic_status_mode, 2, true, msg);
-    }
+      return;
+    default:
+      return;
   }
 
+  // if we had a previous mode,
+  if (currentMode != NULL) {
+    delete(currentMode);
+  }
+
+  // switch active mode
+  currentModeChar = mode;
+  currentMode = newMode;
+
+  // turn on the power supply
+  digitalWrite(PS_OUTPUT, HIGH);
+
+  // tell the world
+  char msg[100] = "Switch:";
+  strcat(msg, currentMode->description());
+  mqttClient.publish(topic_status_mode, 2, true, msg);
+}
+
+void processPendingMessage() {
+
+  char targetModeChar = pendingMessage[0];
+  char* modePayload = pendingMessage + 1;
+
+  if (targetModeChar != currentModeChar) {
+    switchMode(targetModeChar, modePayload);
+  } else {
+    if (currentMode != NULL) {
+      updateMode(modePayload);
+    }
+  }
 }
 
 /* ========================================================================================================
@@ -371,17 +408,21 @@ void checkAndResetWifi()
 }
 
 void loop() {
-  if (currentMode != NULL) {    
-    digitalWrite(PS_OUTPUT, HIGH);
+
+  if (currentMode != NULL) {
     // don't try to send data unless the power supply confirms it's good
     if (digitalRead(PS_CHECK) > 0) {
       currentMode->tick();
-    } else {
     }
   }
+
+  Serial.println(digitalRead(PS_CHECK));
+  delay(20);
+
   checkAndResetWifi();
-  if (hasNewPayload) {
-    processMessage();
-    hasNewPayload = false;
+
+  if (hasUnprocessedMessage) {
+    processPendingMessage();
+    hasUnprocessedMessage = false;
   }
 }
